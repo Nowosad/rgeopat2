f9928394c0d529a188928321221bc84c62586918
Nowosad <tupiszakaczki@gmail.com>
2017-12-21 11:18:47 -0500

    read txt tests added

---
+++ tests/testthat/test-gpat_read_txt.R
@@ -0,0 +1,24 @@
+context("gpat_read_txt")
+test_that("read points", {
+
+  my_points = system.file("rawdata/Augusta2011_points.txt", package="rgeopat2")
+
+  # data is intact
+  expect_equal(nrow(gpat_read_txt(my_distmtx)), 4)
+
+  # bad inputs
+  expect_error(gpat_read_txt("my_invisible_file"))
+
+})
+
+test_that("read polygons", {
+
+  my_polygon = system.file("rawdata/Augusta2011_polygon.txt", package="rgeopat2")
+
+  # data is intact
+  expect_equal(nrow(gpat_read_txt(my_distmtx)), 19)
+
+  # bad inputs
+  expect_error(gpat_read_txt("my_invisible_file"))
+
+})
